95ade2dd8c03d7bf459b71f3795df7d21dc87e57
Ben Copsey <ben@allseeing-i.com>
2009-03-22 11:57:17 +0000

    Remove old XCode project, and add two new ones, one for iPhone, one for Mac This should make things easier - XCode never seemed to like mixing SDKs in a single project Add new file layout

---
+++ iPhone Sample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iPhone
+//
+//  Created by Ben Copsey on 22/03/2009.
+//  Copyright All-Seeing Interactive 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
