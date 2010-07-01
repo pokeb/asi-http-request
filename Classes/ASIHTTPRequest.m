4b480f378bdddd8e1d73bf79842fe2f02625f2cf
Ben Copsey <ben@allseeing-i.com>
2010-07-01 10:12:34 +0100

    Update donors - many thanks!

--- Classes/ASIHTTPRequest.m
+++ Classes/ASIHTTPRequest.m
@@ -24,7 +24,7 @@
 
 // Automatically set on build
 
-NSString *ASIHTTPRequestVersion = @"v1.7-8 2010-06-30";
+NSString *ASIHTTPRequestVersion = @"v1.7-9 2010-06-30";
 
 NSString* const NetworkRequestErrorDomain = @"ASIHTTPRequestErrorDomain";
 
