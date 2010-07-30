b60d4f2f2dec7c47fcb76d8975d69c1f1f714449
Ben Copsey <ben@allseeing-i.com>
2010-07-30 13:46:35 +0100

    Fix nib name in iPad sample (closes gh-67)

--- Classes/ASIHTTPRequest.m
+++ Classes/ASIHTTPRequest.m
@@ -23,7 +23,7 @@
 
 
 // Automatically set on build
-NSString *ASIHTTPRequestVersion = @"v1.7-36 2010-07-27";
+NSString *ASIHTTPRequestVersion = @"v1.7-38 2010-07-30";
 
 NSString* const NetworkRequestErrorDomain = @"ASIHTTPRequestErrorDomain";
 
